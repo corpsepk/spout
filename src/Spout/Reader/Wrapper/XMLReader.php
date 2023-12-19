7228f8bca0b02abc24590c25fbf775b6bdcc4b35
corpsepk <corpsepk@gmail.com>
2023-12-19 12:46:37 +0300

    XMLReader php 8.1 fix

--- src/Spout/Reader/Wrapper/XMLReader.php
+++ src/Spout/Reader/Wrapper/XMLReader.php
@@ -83,6 +83,7 @@ class XMLReader extends \XMLReader
      * @throws \Box\Spout\Reader\Exception\XMLProcessingException If an error/warning occurred
      * @return bool TRUE on success or FALSE on failure
      */
+    #[ReturnTypeWillChange]
     public function read()
     {
         $this->useXMLInternalErrors();
